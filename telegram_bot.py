5973e9170159c038a6c4bfbb3516052b0a6f62c0
DeusProtivogas <kirill2689@gmail.com>
2024-06-04 18:53:42 +0300

    refactored repeating functions

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,12 +2,11 @@ import os
 import logging
 
 from dotenv import load_dotenv
-from google.cloud import dialogflow
-from google.cloud import api_keys_v2
-
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+from dialogflow_utils import create_api_key, detect_intent_texts
 
 # Enable logging
 logging.basicConfig(
@@ -15,57 +14,6 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-
-
-def create_api_key(project_id: str, suffix: str):
-    client = api_keys_v2.ApiKeysClient()
-
-    key = api_keys_v2.Key()
-    key.display_name = f"My first API key - {suffix}"
-
-    # Initialize request and set arguments.
-    request = api_keys_v2.CreateKeyRequest()
-    request.parent = f"projects/{project_id}/locations/global"
-    request.key = key
-
-    # Make the request and wait for the operation to complete.
-    response = client.create_key(request=request).result()
-
-    # print(f"Successfully created an API key: {response.name}")
-    # For authenticating with the API key, use the value in "response.key_string".
-    # To restrict the usage of this API key, use the value in "response.name".
-    return response
-
-def detect_intent_texts(project_id, session_id, text, language_code='en-US'):
-    """Returns the result of detect intent with texts as inputs.
-
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
-
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    # print("Session path: {}\n".format(session))
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    answer = response.query_result.fulfillment_text
-    print("Fulfillment text: {}\n".format(answer))
-    return answer
 
 
 # Define a few command handlers. These usually take the two arguments update and
--- vk_bot.py
+++ vk_bot.py
@@ -5,41 +5,7 @@ from dotenv import load_dotenv
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from google.cloud import dialogflow
-from google.cloud import api_keys_v2
-from google.cloud.api_keys_v2 import Key
-
-def create_api_key(project_id: str, suffix: str) -> Key:
-    client = api_keys_v2.ApiKeysClient()
-
-    key = api_keys_v2.Key()
-    key.display_name = f"My first API key - {suffix}"
-
-    request = api_keys_v2.CreateKeyRequest()
-    request.parent = f"projects/{project_id}/locations/global"
-    request.key = key
-
-    response = client.create_key(request=request).result()
-
-    return response
-
-def detect_intent_texts(project_id, session_id, text, language_code='en-US'):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    if response.query_result.intent.is_fallback:
-        return ""
-    answer = response.query_result.fulfillment_text
-    return answer
+from dialogflow_utils import create_api_key, detect_intent_texts
 
 
 
